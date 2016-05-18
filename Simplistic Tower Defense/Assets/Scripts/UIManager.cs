6623f39a522970fa8d0f184caa4b30ec9e5c4d09
Kevin Biehler <kevin_biehler@hotmail.com>
2016-05-18 13:04:03 +0200

    added new tower and test menu scene

---
+++ Simplistic Tower Defense/Assets/Scripts/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
